cca56f68171f731bade9b9404f871fad13f02869
Diana Brik <di.brik@yandex.kz>
2023-02-03 20:38:10 +0600

    Seminar 29.01.23 Task 64

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿void OutputNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        OutputNumbers(m + 1, n);
+        Console.Write($"{m} ");
+    }
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+OutputNumbers(m, n);
